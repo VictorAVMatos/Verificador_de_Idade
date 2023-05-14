16d785d7be3a8f751618f57aaa75eb51a993be0a
Victor Alexandre Vieira de Matos <112444309+VictorAVMatos@users.noreply.github.com>
2023-05-13 22:12:24 -0300

    Add files via upload

---
+++ Identificador_de_idade.java
@@ -0,0 +1,17 @@
+public class Identificador_de_idade {
+    public static void man (String [] args){
+
+        int idade = 7;
+        if (idade <= 7) {
+            System.out.println ("É uma criança");
+        }else if (idade > 7 && idade <= 17) {
+            System.out.println ("É um (a) adolescente");
+        } else if (idade > 18 && idade <= 65){
+            System.out.println ("É um (a) adulto (a)");
+        }else {
+            System.out.println ("Está na melhor idade")
+        }
+
+        
+    }
+}
